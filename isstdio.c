713a6d69cb73d74939be8753231af7e822c62b9d
Dylan Lesko <dylanlesko@embarqmail.com>
2015-03-13 15:36:28 -0400

    isstdio bullshit

---
+++ isstdio.c
@@ -0,0 +1,34 @@
+#include <stdlib.h> /* needed for exit() */
+#include <unistd.h> /* needed for isatty() */
+#include <stdio.h> /* needed for printf() */
+#include "types.h"
+
+int is_std_i()
+{
+	if (isatty(0)) 
+	{
+		printf("the standard input is from a terminal\n");
+		return true;
+	}
+	else
+	{
+		printf("the standard input is NOT from a terminal\n");
+		return false;
+	}
+}
+
+int is_std_o()
+{
+	if (isatty(1))
+	{
+		printf("the standard output is to a terminal\n");
+		return true;
+	}
+	else
+	{ 
+		printf("the standard output is NOT to a terminal\n");
+		return false;
+	}
+	
+
+}
