8f0d759593302df8af06c53621a20edfba53755c
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2015-03-16 15:52:42 -0400

    starting to work on the pipe

--- exec/main.c
+++ exec/main.c
@@ -8,31 +8,17 @@ int main() {
 
 	LL *first = (LL *)malloc(sizeof(LL));
 	LL *second = (LL *)malloc(sizeof(LL));
-	LL *third = (LL *)malloc(sizeof(LL));
-	LL *fourth = (LL *)malloc(sizeof(LL));
-	LL *fifth = (LL *)malloc(sizeof(LL));
 
 	strcpy(first->command,"ls");
 	strcpy(first->args[0],"/home/nikhil/Documents");
 	first->next = second;
 
 
-	strcpy(second->command,"ls");
-	strcpy(second->args[0],"/home/nikhil/Downloads");
-	second->next = third;
+	strcpy(second->command,"grep");
+	strcpy(second->args[0],"Win");
+	second->next = NULL;
 	
-	strcpy(third->command,"ls");
-	strcpy(third->args[0],"/home/nikhil/Desktop");
-	third->next = fourth;
-
-	strcpy(fourth->command,"ls");
-	strcpy(fourth->args[0],"/home/nikhil/Pictures");
-	fourth->next = fifth;
-
-	strcpy(fifth->command,"ls");
-	strcpy(fifth->args[0],"/home/nikhil/Public");
-	fifth->next = NULL;
-
+	
 	pid_t childPid;
 	int status;
 
@@ -42,26 +28,49 @@ int main() {
 	int i;
 	i = 0;
 
-	LL *head;
-	head = first;
+	LL *progOne, *progTwo;
+	progOne = first;
 
-	while(head != NULL) {
+	int fd[2]; // file descriptors for redirection
+	pipe(fd); // create the pipe between the two file descriptors
 
+	while(progOne != NULL) {
+
+
+		if(progTwo != NULL) {
+			int fd[2]; // file descriptors for redirection
+			pipe(fd); // create the pipe between the two file descriptors
+
+			runsource(fd,progOne);
+			rundest(fd,progTwo);
+			close(fd[0]);
+			close(fd[1]);
+		}
+	
+
+
+		/*
 		if((childPid = fork()) == 0) {
 			char *Args[51];
-			prepForExec(Args,head);
+			prepForExec(Args,progOne);
 	
 			execvp(Args[0],Args);
 		} else {
 			pids[i] = childPid;
 			i++;
-			head = head->next;
+			progOne = progOne->next;
 		}
+		*/
 	}
 
+	while((pid = wait(&status)) != -1)
+		fprintf(stderr,"Process %d exits with %d\n",pid,WEXITSTATUS(status));
+
 	int j;
-	for(j = 0; j < commandNum; j++) 	
+	for(j = 0; j < commandNum; j++) {	
 		waitpid(pids[j],&status,0);
+		printf("process %d exits with status %d\n",pids[j],status);
+	}
 
 	destroy(first);
 
