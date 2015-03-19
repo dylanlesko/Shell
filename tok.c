bc4494296783317e77f0562edc68c26155b5442c
Dylan Lesko <dylan@lesko.io>
2015-03-18 23:23:42 -0400

    Update tok.c

--- tok.c
+++ tok.c
@@ -1,169 +1,189 @@
 #include "defs.h"
+
 #include "types.h"
+
 #include <string.h>
+
 #include <stdio.h>
 
-/*
- * 1) Create Tokenizer
- * 		a) Generate tokens based off of quotations
- * 		b) Parse tokens based off of pipe
- * 		c) Parse the final subset based off of spaces and tabs
- * 
- * 2) 
- * 
- * 
- * 
- * 
- */
- 
-int shell_tok( char* args, LList** list )
-{	 
-	if( args == NULL )
-	{
+
+
+int shell_tok( char* args, LList** list )	{	 
+
+	if( args == NULL )	{
+
 		return 1;
+
 	}
+
+
 
 	char *iter = args;
+
 	char token[2048];
+
 	int i = 0;
-	int j = 0;
+
 	int offset = 0;
-	char fuck[50][2048];
+
 	
-	//printf("len: %zu\nlen: %zu", strlen(args), strlen(iter));
-	
-	while( *iter )
-	{
-		//printf("c: %c\n", *iter);
-		if( (*iter) == '\"' )			/* match double quotes 		*/
-		{
+
+	LList *new_node;
+
+
+
+
+
+	/* Loop through each char to null char*/
+
+	while( *iter )	{
+
+		new_node = (LList*)malloc(sizeof(LList));
+
+		/* match double quotes */
+
+		if( (*iter) == '\"' || (*iter) == '\'' )	{
+
+
+
+			offset = shell_tok_match_len( iter, (*iter) );
+
 			iter++;
-			offset = shell_tok_dquote( iter );
-			if( offset == 0 )
-			{
+
+
+
+			/* if no matching quote is found */
+
+			if( offset == 0 )	{
+
 				printf("mismatch\n");
+
 			}
-			else
-			{
-				while( offset > 1 && *iter )
-				{
+
+			else	{
+
+				while( offset > 1 && *iter )	{
+
 					token[i] = (*iter);
+
 					iter++;
+
 					i++;
+
 					offset--;
+
 				}
+
 				iter++;
+
 			}
+
 			offset = 0;
-			printf("token: %s\n", token);
-			strcpy( fuck[j], token );
-			j++;
+
+
+
 			memset(token, 0, 2048);
-			i = 0;		}
-		else if( (*iter) == '\'' )		/* match single quotes 		*/
-		{
-			iter++;
-			offset = shell_tok_squote( iter );
-			if( offset == 0 )
-			{
-				printf("mismatch\n");
-			}
-			else
-			{
-				while( offset > 1 && *iter )
-				{
-					token[i] = (*iter);
-					iter++;
-					i++;
-					offset--;
-				}
-				iter++;
-			}
-			offset = 0;
-			printf("token: %s\n", token);
-			strcpy( fuck[j], token );
-			j++;
-			memset(token, 0, 2048);
-			i = 0;
+
+			i = 0;		
+
 		}
-		else if( (*iter) == '|' )		/* token at pipe 			*/
-		{
-			if( i == 0)
-			{
+
+		/* pipe found, should end command-argument block here */
+
+		else if( (*iter) == '|' )	{
+
+			if( i == 0)	{
+
 				iter ++;
-			}
-			else
-			{
-				iter++;
-				printf("token: %s\n", token);
 
-				//ins_node( fuck, list );
+			}
+
+			else	{
+
+				llist_ins( list, token );
+
+				iter++;
 
 				memset(token, 0, 2048);
+
 				i = 0;
+
 			}
+
 		}
-		else if( (*iter) == ' ' || (*iter) == '\t' )	/* token at space or tab 	*/
-		{
-			if( i == 0)
-			{
+
+		/* space or tab found */
+
+		else if( (*iter) == ' ' || (*iter) == '\t' )	{
+
+			if( i == 0)	{
+
 				iter++;
+
 			}
-			else
-			{
+
+			else	{
+
 				iter++;
-				printf("token: %s\n", token);
+
 				memset(token, 0, 2048);
+
 				i = 0;
+
 			}
+
 		}
-		else
-		{
+
+		/* add char to the token string */
+
+		else	{
+
 			token[i] = (*iter);
+
 			iter++;
+
 			i++;
+
 		}
-		
-
-
 
 	}
-	printf("token: %s\n", token);
+
+
+
+	if( strcmp( token, "\0" ) != 0 )
+
+		printf("token: %s\n", token);
+
+	memset(token, 0, 2048);
+
+
 
 }
 
-int shell_tok_dquote( char* cmd_line )
-{
+
+
+int shell_tok_match_len( char* cmd_line, char match )	{
+
+
+
 	char *iter = cmd_line;
+
 	int char_count = 0;
 
-	while( *iter )
-	{
+
+
+	while( *iter )	{
+
 		char_count++;
-		if( ( *iter ) == '\"' )
-		{
-			return char_count;
-		}
-	
-					
-		iter++;
-	}
-	return 0;
-}
 
-int shell_tok_squote( char* cmd_line )
-{
-	char *iter = cmd_line;
-	int char_count = 0;
+		if( ( *iter ) == match )
 
-	while( *iter )
-	{
-		char_count++;		
-		if( ( *iter ) == '\'' )
-		{
 			return char_count;
-		}
+
 		iter++;
+
 	}
+
 	return 0;
+
 }
