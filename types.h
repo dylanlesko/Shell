671bcd4548623246c29e8dcd749ced4d89e24e5f
Dylan Lesko <dylan@lesko.io>
2015-03-18 23:23:54 -0400

    Update types.h

--- types.h
+++ types.h
@@ -1,34 +1,70 @@
 #ifndef TYPES_H
+
 #define TYPES_H
 
 
+
+
+
 /* */
+
 typedef enum { 
+
 	false,
+
 	true
+
 } boolean;
 
 
 
+
+
+
+
 /* */
+
 typedef struct LList
+
 {
+
 	char command[2048];
+
 	char args[50][2048];
+
+	int arg_count;
+
 	struct LList *next;
+
+
 
 } LList;
 
 
-/* */
-typedef char* stringStructPtr;
+
+
 
 /* */
+
+typedef char* stringStructPtr;
+
+
+
+/* */
+
 #define FREE(ptr) 	\
+
 do{ 				\
+
 free((ptr)); 		\
+
 (ptr)=NULL; 		\
+
 }while(0) 
 
 
+
+
+
 #endif
+
